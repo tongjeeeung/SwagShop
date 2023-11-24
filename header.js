2e34c25315457c5e99f1d2e739be6f82b2a8de68
Tong <tongjeeeung@gmail.com>
2023-11-25 01:05:36 +0300

    fix header2

--- header.js
+++ header.js
@@ -8,6 +8,7 @@ class Header {
         <img 
           class="header__logo-image"
           src="../images/nMetfr_2abA.jpg"
+          src="../../images/nMetfr_2abA.jpg"
           alt="Лого сайта"
           >
       </a>
